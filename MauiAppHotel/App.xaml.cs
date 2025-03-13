291f1f9a460376c9527d38ee9b40814fbe6eec9e
0200832221028 <0200832221028@LAB03-02>
2025-03-12 22:05:33 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
+
+{
+   
+        public partial class App: Application
+        {
+            public List<Quarto> lista_quartos = new List<Quarto>()
+            {
+                new Quarto()
+                {
+                    Descricao = "Suite Super Luxo",
+                    ValorDiariaAdulto = 110.0,
+                    ValorDiariaCrianca = 55.0
+                },
+                new Quarto()
+                {
+                    Descricao = "Suite Luxo",
+                    ValorDiariaAdulto = 80.0,
+                    ValorDiariaCrianca = 40.0
+                },
+                new Quarto()
+                {
+                    Descricao = "Suite Single",
+                    ValorDiariaAdulto = 50,
+                    ValorDiariaCrianca = 25
+                }
+                new Quarto()
+                {
+                    Descricao = "Suite Crise",
+                    ValorDiariaAdulto = 25,
+                    ValorDiariaCrianca = 12.5
+                }
+            };
+            public App()
+            {
+                InitializeComponent();
+
+                MainPage = new NavigationPage(new Views.());
+            }
+            protected override Window CreateWindow(IActivationState? activationState)
+            {
+                var window = base.CreateWindow(activationState);
+
+                window.Width = 400;
+                window.Height = 600;
+
+                return Window;
+            }
+        }
+    }
---
+++ MauiAppHotel/Models/Hospedagem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppHotel.Models
+{
+    public class Hospedagem
+    {
+        public Quarto QuartoSelecionado { get; set; }
+
+        public double QntAdulto { get; set; }
+
+        public double QntCriancas { get; set; }
+        public DateTime DataCheckIn { get; set; }
+        public DateTime DataCheckout { get; set; }
+        public double Estadia
+        {
+            get => DataCheckout.Subtract(DataCheckIn).Days;
+
+        }
+        public double ValorTotal
+        {
+            get
+            {
+                double Valor_adultos = QntAdulto * QuartoSelecionado.ValorDiariaAdulto;
+                double Valor_criancas = QntCriancas * QuartoSelecionado.ValorDiariaCrianca;
+
+                double total = (Valor_adultos + Valor_criancas) * Estadia;
+
+                return total;
+            }
+        }//fechamento da classes
+    }//fechamento namespace
+}
